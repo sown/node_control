d1a18944e7c4af77f72ddaf0d950a94d8f1df624
Colin Williams <github@crwilliams.co.uk>
2012-05-10 21:05:10 +0200

    Corrected a2enmod command.

--- setup.bash
+++ setup.bash
@@ -9,7 +9,7 @@ sudo sed -i "s/`lsb_release -cs`/maverick/" /etc/apt/sources.list.d/kohana-stabl
 sudo apt-get update
 sudo apt-get install git-core libkohana3.2-core-php libkohana3.2-mod-auth-php libkohana3.2-mod-cache-php libkohana3.2-mod-codebench-php libkohana3.2-mod-database-php libkohana3.2-mod-image-php libkohana3.2-mod-orm-php libkohana3.2-mod-unittest-php mysql-client mysql-server php5-mysql libmysqlclient15-dev
 
-sudo a2enmod mod_rewrite
+sudo a2enmod rewrite
 
 cd /srv/www/default
 
