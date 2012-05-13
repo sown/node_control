700b9bfa8d41d0a75c646f4164ec9484371f662a
Colin Williams <github@crwilliams.co.uk>
2012-05-13 23:41:44 +0100

    Corrected superclass reference.

--- application/classes/model/type/nodedeploymenttype.php
+++ application/classes/model/type/nodedeploymenttype.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_Type_NodeDeploymentType extends Model_Type_EnumType
+class Model_Type_NodeDeploymentType extends Model_Type_Enum
 {
     protected $name = 'nodedeploymenttype';
     protected $values = array('home', 'campus');
