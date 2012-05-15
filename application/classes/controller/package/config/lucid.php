f8444f53e27eeaf2f36164c15edc658202e1f888
Colin Williams <github@crwilliams.co.uk>
2012-05-15 23:32:07 +0100

    More changes relating to lucid config system.

---
+++ application/classes/controller/package/config/lucid.php
@@ -0,0 +1,62 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Package_Config_Lucid extends Controller
+{
+	public function action_default()
+	{
+		$package      = $this->request->param('package');
+		$version      = $this->request->param('version');
+		$request_name = $this->request->param('request_name');
+
+		if (substr($package, 0, 13) != 'sown_openwrt_')
+			Package_Config::send_shell_script("echo \"Unsupported package '$package'\" >&2; return 1\n");
+		
+		$short = substr($package, 13);
+		$classname = 'Package_Config_Lucid_' . ucfirst($short);
+		
+		if (! class_exists($classname))
+			Package_Config::send_shell_script("echo \"Unable to find configure script for package '$package'.\" >&2; return 1\n");
+		
+		
+		if (! isset($classname::$supported[$request_name]))
+			Package_Config::send_shell_script("echo \"Request '$request_name' for package '$package' not supported.\" >&2; return 1\n");
+		
+		
+		$versions = $classname::$supported[$request_name];
+		// Go through backwards, newest version first
+		$found = FALSE;
+		for ($i=count($versions) -1; $i >= 0 ; $i--) {
+			// Check we match the minimum version
+			if ((isset($versions[$i]['>']) && 
+					version_compare($version, $versions[$i]['>'], '>')) ||
+				(isset($versions[$i]['>=']) &&
+					version_compare($version, $versions[$i]['>='], '>=')))
+			{
+				// If there is a max version, and we do not match it, skip
+				if ((isset($versions[$i]['<']) && 
+						! version_compare($version, $versions[$i]['<'], '<')) ||
+					(isset($versions[$i]['<=']) &&
+						! version_compare($version, $versions[$i]['<='], '<=')))
+					continue;
+				$found = $i;
+				break;
+			}
+		}
+
+		if ($found === FALSE)
+		{
+			SOWN::send_irc_message('Node config: client '.Request::$client_ip.' requested a resource which could not be found: '."$package:$version:$request_name.");
+			Package_Config::send_shell_script("echo \"Request '$request_name' at version '$version' for package '$package' not supported.\" >&2; return 1\n");
+		}
+		
+		$node = $classname::get_node($this->request);
+		if ($node == null)
+		{
+			SOWN::send_irc_message('Node config: Unable to determine node from client '.Request::$client_ip.'.');
+			Package_Config::send_shell_script("echo \"Unable to determine node from request for '$request_name' at version '$version' for package '$package'.\" >&2; return 1\n");
+		}
+		
+		//SOWN::send_irc_message('calling '.$classname.'::'.$versions.'['.$found.']["method"]('.$node.', '.$version.');');
+		$classname::$versions[$found]['method']($node, $version);
+	}
+}
---
+++ application/classes/package/config/lucid/tunnel.php
@@ -0,0 +1,83 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Package_Config_Lucid_Tunnel extends Package_Config
+{
+	const package_name = "sown_openwrt_tunnel";
+
+	public static $supported = array(
+		'config_openvpn' => array(
+			array(
+				'>=' => '0.1.78',
+				'method' => 'config_openvpn_v0_1_78'
+			),
+		),
+	);
+
+	public static function config_openvpn_v0_1_78(Model_Node $node)
+	{
+		$ep = $node->vpnEndpoint;
+$conf = <<< EOB
+# Comments are preceded with '#' or ';'
+
+# Accept Connectionions on this port.
+port {$ep->port}
+
+# sown-vpn is correctly configured to use udp
+proto {$ep->protocol}
+
+# sown-vpn uses tap tunnels
+dev tap{$ep->id}
+
+# Locations of SSL files
+ca /etc/openvpn/package_managment/ca.crt
+cert /etc/openvpn/package_managment/vpnserver.crt
+key /etc/openvpn/package_managment/vpnserver.key
+
+# Diffie Hellman Parameters
+dh /etc/openvpn/dh1024.pem
+
+# Use this subnet for this client
+server {$ep->IPv4->get_network_address()} {$ep->IPv4->get_subnet_mask()}
+
+# Push these routes to the client
+push "route 10.12.0.0 255.254.0.0"
+push "route 152.78.189.82 255.255.255.255"
+push "route 152.78.189.90 255.255.255.255"
+
+# Tell the client it must tell us when it is
+# disconnecting. This prevents time-out errors
+# and means routes come down at the right time
+# This only works with udp.
+;explicit-exit-notify
+
+# Push these configurations to the client
+push "dhcp-option DNS 10.13.0.254"
+
+# Allow clients to see each other
+# This is useless in sown-vpns configuration
+client-to-client
+
+# Send keep-alives
+keepalive 10 120
+
+# Maximum number of clients for this server
+
+# Downgrade priveleges after initialization
+user nobody
+group nogroup
+
+# Preserve as much as possible between restarts
+persist-key
+persist-tun
+
+# Keep per-server log files
+log /var/log/openvpn/server{$ep->id}.log
+status /var/log/openvpn/server{$ep->id}-status.log
+
+# Set logging verbosity to 3
+verb 3
+EOB;
+
+		echo $conf;
+	}
+}
