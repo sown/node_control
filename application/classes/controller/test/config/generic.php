3516a55cf2acba1aa4529e27fa4b05f09694a568
Colin Williams <github@crwilliams.co.uk>
2012-06-25 00:15:00 +0100

    Added server list to /test page.

--- application/classes/controller/test/config/generic.php
+++ application/classes/controller/test/config/generic.php
@@ -23,6 +23,13 @@ class Controller_Test_Config_Generic extends Controller
 			echo "<hr/>";
 			echo $node->toString()."<br/>";
 		}
+
+		$repository = Doctrine::em()->getRepository('Model_Server');
+		foreach($repository->findAll() as $server)
+		{
+			echo "<hr/>";
+			echo $server->toString()."<br/>";
+		}
 	}
 
 	private function storeKeys($hostname, $os)
