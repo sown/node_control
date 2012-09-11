387fe37bc09b40a1f8abd77a300c4198cf1fa49f
David R Newman <drn@ecs.soton.ac.uk>
2012-09-11 03:31:16 +0100

    Fixed table code for change_password view form

--- application/views/pages/change_password.php
+++ application/views/pages/change_password.php
@@ -10,9 +10,9 @@ foreach($info as $class => $classinfo)
 <form method="POST">
 	<table>
 		<tr><th>Username:</th><td><?= $username ?></td></th>
-		<tr><th>Current Password:</th><td><input name='oldpassword' type='password' /></td></th>
-		<tr><th>New Password:</th><td><input name='password1' type='password' /></td></th>
-		<tr><th>Confirm Password:</th><td><input name='password2' type='password' /></td></th>
-		<tr><th></th><td><input type='submit' /></td></th>
+		<tr><th>Current Password:</th><td><input name='oldpassword' type='password' /></td></tr>
+		<tr><th>New Password:</th><td><input name='password1' type='password' /></td></tr>
+		<tr><th>Confirm Password:</th><td><input name='password2' type='password' /></td></tr>
+		<tr><th></th><td><input type='submit' name='submit' value='Change Password' /></td></tr>
 	</table>
 </form>
