386d15b0b0ffe39c9acf167b5eeb74c01cbc337d
David R Newman <drn@ecs.soton.ac.uk>
2012-09-24 23:43:02 +0100

    Fixed status controller to extend Controller rather than Controller_AbstractAdmin

--- application/classes/controller/status/config/generic.php
+++ application/classes/controller/status/config/generic.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Status_Config_Generic extends Controller_AbstractAdmin
+class Controller_Status_Config_Generic extends Controller
 {
 	public function action_default()
 	{
