b3b1d174c417fd72f93c1afc4aea0e17ed4e81ad
James Morse <jsmm104@ecs.soton.ac.uk>
2012-11-12 19:37:20 +0000

    Added check code for whether the openvpn server is running

---
+++ application/classes/check/openvpnrunning.php
@@ -0,0 +1,40 @@
+<?php
+class Check_OpenvpnRunning extends Check
+{
+        protected $file;
+        protected $name;
+
+
+	public function Check_OpenvpnRunning($host)
+	{
+		$this->name = "OPENVPN";
+		$this->file = "/srv/www/tmp/openvpn";
+
+                $last = time() - (6 * 60);
+                if (filemtime($this->file) < $last) {
+                        $this->message = "File stale last modified: " . date("c",filemtime($this->file));
+                        return;
+                }
+
+		$port = $host->vpnEndpoint->port;
+
+                $handle = fopen($this->file, "r");
+                while(!feof($handle)) {
+                        $line = fgets($handle);
+                        if (strpos($line,"*:" . $port) !== false) {
+                                $this->code = Check::OK;
+                                $this->message = "Openvpn Server Running on port ".$port;
+                                fclose($handle);
+                                return;
+                        }
+                }
+
+                fclose($handle);
+
+
+		$this->code = Check::CRITICAL;
+		$this->message = "Openvpn Server NOT Running on port ".$port;
+
+
+	}
+}
