65988d92465728c3e4a0e22c908beca1c404381f
David R Newman <drn@ecs.soton.ac.uk>
2012-11-14 13:57:17 +0000

    Updated the way doctrine sets up a database connection to pull credentials out of database.php settings file

--- application/classes/mysql-dbo.php
+++ application/classes/mysql-dbo.php
@@ -2,7 +2,10 @@
 
 //$dbh = new PDO("mysql:dbname=node_config;host=localhost", "auth-https", "6itL6cbuwz=fd");
 global $dbh;
-$dbh = new PDO("mysql:dbname=sown_data;host=localhost", "root", "") or die("WTF");
+$dbconfig = Kohana::$config->load('database')->get('default');
+$dbconn = $dbconfig['connection'];
+//$dbh = new PDO("mysql:dbname=sown_data;host=localhost", "root", "") or die("WTF");
+$dbh = new PDO($dbconfig['type'] . ":dbname=" . $dbconn['database'] . ";host=" . $dbconn['hostname'], $dbconn['username'], $dbconn['password']) or die("Database credentials provided to Doctrine were not correct");
 
 function query($q, $params, $type) {
         global $dbh;
