053e9f95b6189521d497d542d93580bc4d01b921
David R Newman <drn@ecs.soton.ac.uk>
2013-05-24 16:43:04 +0100

    Fixed date mangling for certificate expiry check.

--- application/classes/check/certexpiry.php
+++ application/classes/check/certexpiry.php
@@ -7,12 +7,11 @@ class Check_CertExpiry extends Check
 		$this->code = Check::OK;
 		$this->message = "Certificate will expiry on sometime.";
 		$certificate = openssl_x509_parse($host->certificate->publicKey);
-		$valid_to = str_replace("Z", "", $certificate['validTo']);
-		$valid_to = "20".substr($valid_to,4,2)."-".substr($valid_to,2,2)."-".substr($valid_to,4,2)." ".substr($valid_to,6,2).":".substr($valid_to,8,2).":".substr($valid_to,10,2);
-		$unix_valid_to = strtotime($valid_to);
+		$unix_valid_to = $certificate['validTo_time_t'];
+		$valid_to = date("Y-m-d H:i:s", $unix_valid_to);
 		$unix_30days_later = strtotime("+30 days");
 		$unix_90days_later = strtotime("+90 days");
-		
+
 		if ($unix_30days_later > $unix_valid_to) {
 	                $this->code = Check::CRITICAL;
 		}
@@ -22,7 +21,7 @@ class Check_CertExpiry extends Check
 		else {
 			$this->code = Check::OK;
                 }
-		error_log($this->code." - ".$this->message);
+		error_log($this->code." - ".$this->message); 
 		$this->message = "Certificate will expiry on $valid_to.";
 	}
 }
