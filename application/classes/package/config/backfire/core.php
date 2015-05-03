f7165969dbcc52e5b281a171ccb099798269f97e
James Morse <jsmm104@ecs.soton.ac.uk>
2015-05-03 14:01:54 +0100

    added config_ca to /etc/config/sown_core, specifies the local path to the web server https ca

--- application/classes/package/config/backfire/core.php
+++ application/classes/package/config/backfire/core.php
@@ -131,6 +131,7 @@ class Package_Config_Backfire_Core extends Package_Config
 			'node' => array(
 				array(
 					'config_URL' => Kohana::$config->load('system.default.node_config.url').'/package/config/backfire/',
+					'config_ca' => '/etc/sown/node_control_2012.crt',
 					'hostname'   => $node->hostname,
 					'node_name'  => $node->name,
 					'id'         => $node->id,
