78b225f13442393a5d37a0028795d043d50e0d01
David R Newman <drn@ecs.soton.ac.uk>
2018-02-10 09:33:54 +0000

    Added check for whether there are ungenerated certificates.

---
+++ application/classes/check/certscreated.php
@@ -0,0 +1,25 @@
+<?php
+class Check_CertsCreated extends Check
+{
+
+	public function Check_CertsCreated($host)
+	{
+		$this->code = Check::OK;
+		$this->message = "All certificates present2.";
+
+		$certs = Doctrine::em()->getRepository('Model_Certificate')->findAll();
+		$ids = array();
+		foreach ($certs as $cert) 
+		{
+			if ($cert->publicKey == null && $cert->current)
+			{
+				$ids[] = $cert->id;
+			}
+		}
+		if (sizeof($ids) > 0)
+		{	
+			$this->code = Check::CRITICAL;
+			$this->message = "There are ".sizeof($ids)." certificates missing with the following IDs: ".implode(", ", $ids);
+		}
+	}
+}
